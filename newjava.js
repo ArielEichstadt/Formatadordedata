7ae2773cda98523444794654082c82db43b438bb
ArielEichstadt <125396879+ArielEichstadt@users.noreply.github.com>
2023-05-16 14:33:17 -0300

    Data formatada em Português

---
+++ newjava.js
@@ -0,0 +1,17 @@
+function bemvindo(){
+    let nome = window.prompt("digite seu nome")
+    document.getElementById('IDLABEL').innerHTML = nome   
+}
+
+function data(){
+    let date = new Date();
+    let options = { 
+
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric' }
+       
+
+    let formatedDate = date.toLocaleDateString('pt-BR', options);
+    document.getElementById('IDDATA').textContent = formatedDate;
+}
